f96ae59bf2293bc8a51210ce8570786649ac63a3
bahtiyar-85 <b.ilyazov@gmail.com>
2021-12-12 12:10:37 +0600

    pagination adminPage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <div>
       <Navbar />
-      <video className="background-video" muted autoPlay preload="auto" loop playsinline>
+      <video className="background-video" muted autoPlay preload="auto" loop >
                 {/* <source type="video/webm" src="https://thumbs.gfycat.com/MixedTartCalf-mobile.mp4"/> */}
                 <source type="video/mp4" src="https://giant.gfycat.com/ColorlessQuerulousDoe.mp4"/>
             </video>
