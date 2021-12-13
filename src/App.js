eb856973666d6f3a72ffd39dab009097743b88ac
bahtiyar-85 <b.ilyazov@gmail.com>
2021-12-13 14:19:48 +0600

    create card for Home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
 import AdminPage from './components/AdminPage/AdminPage';
 import Header from './components/Header/Header';
 import ProductsContextProvider from './contexts/productsContext';
@@ -7,7 +8,6 @@ import Footer from './components/Footer/Footer'
 
 import './App.css'
 import Home from './components/Home/Home';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
